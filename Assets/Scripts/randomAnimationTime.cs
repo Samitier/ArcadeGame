82cba50624760aa532111213e76c4fa9e7adfb45
Jordi Fuentes Bonette <jordi.bonette@gmail.com>
2015-07-19 20:37:08 +0200

    finished background

---
+++ Assets/Scripts/randomAnimationTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomAnimationTime : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		Invoke ("playAnimation",Random.Range( 5.0f, 5.0f));
+	}
+	
+	// Update is called once per frame
+	void playAnimation () {
+		anim.Play ("smallSmoke_");
+		Invoke ("playAnimation",Random.Range (5.0f, 5.0f));
+	}
+}
